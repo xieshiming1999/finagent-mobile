7d654e82529b1b2c6965f193bb274f3eaf3a7d92
shiming.xie <shiming.v.xie@gmail.com>
2026-07-02 09:34:35 +0800

    Add macOS Flutter and Pod project config

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import file_picker
+import flutter_js
+import shared_preferences_foundation
+import sqlite3_flutter_libs
+import url_launcher_macos
+import webview_flutter_wkwebview
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FlutterJsPlugin.register(with: registry.registrar(forPlugin: "FlutterJsPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+}
